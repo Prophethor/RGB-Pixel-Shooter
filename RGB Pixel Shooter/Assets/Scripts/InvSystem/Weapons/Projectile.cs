b1cfd29779dd5e9a7e79ef376e8a3578d180a7ca
Veloxis <velox1s@yahoo.com>
2020-06-04 12:19:37 +0200

    BloodSplatter fix

--- RGB Pixel Shooter/Assets/Scripts/InvSystem/Weapons/Projectile.cs
+++ RGB Pixel Shooter/Assets/Scripts/InvSystem/Weapons/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour {
 
     [SerializeField]
     public RGBDamage damage;
-    public GameObject bloodSplater; 
+    public GameObject bloodSplatter;
     // Array of bullet sprites in order: Red, Green, Blue
     public Sprite[] bulletSprites = new Sprite[3];
 
@@ -23,39 +23,11 @@ public class Projectile : MonoBehaviour {
         sr.sprite = bulletSprites[(int) color];
     }
 
-    public void SpawnBloodSplater (Vector3 spawnPosition)
-    {
+    public void SpawnBloodSplater (Vector3 spawnPosition) {
         Vector3 offsetVector = new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), 0);
 
-        if (damage.color == RGBColor.BLUE)
-        {
-            bloodSplater.GetComponent<SpriteRenderer>().color = Color.blue;
-        }
-        if (damage.color == RGBColor.RED)
-        {
-            bloodSplater.GetComponent<SpriteRenderer>().color = Color.red;
-        }
-        if (damage.color == RGBColor.GREEN)
-        {
-            bloodSplater.GetComponent<SpriteRenderer>().color = Color.green;
-        }
-        /*
-        switch (color)
-        {
-            case RGBColor.RED:
-                bloodSplater.GetComponent<SpriteRenderer>().color = Color.red;
-                break;
-            case RGBColor.GREEN:
-                bloodSplater.GetComponent<SpriteRenderer>().color = Color.green;
-                break;
-            case RGBColor.BLUE:
-                bloodSplater.GetComponent<SpriteRenderer>().color = Color.blue;
-                break;
-            default:
-                break;
-                 }*/
-        Instantiate(bloodSplater, spawnPosition + offsetVector, Quaternion.identity);
-   
+        GameObject splatterObject = Instantiate(bloodSplatter, spawnPosition + offsetVector, Quaternion.identity);
 
+        splatterObject.GetComponent<SpriteRenderer>().color = damage.color.GetColor();
     }
 }
