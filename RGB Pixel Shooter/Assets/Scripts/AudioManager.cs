f881943d5c6aeab6ac93af980262056c8ae637c6
Stevan Stojkovic <truper93@gmail.com>
2020-07-01 14:50:32 +0200

    Revert "Merge branch 'Steven_Development' of https://gitlab.com/Supahtrupah/rgb-pixel-shooter into Steven_Development"
    This reverts commit d09bb1f46d6cb37621bbede0150724b498026a93, reversing
    changes made to 9d8be3b888b6ad24a828caec964c0474ba682b7c.

---
+++ RGB Pixel Shooter/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager instance;
+
+    private HashSet<AudioClip> audioClips;
+    private AudioSource musicSource;
+
+    private void Awake () {
+        if (instance != null) {
+            Destroy(gameObject);
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        audioClips = new HashSet<AudioClip>();
+
+        musicSource = GetComponent<AudioSource>();
+
+        if (musicSource == null) {
+            musicSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySoundtrack (AudioClip soundtrack) {
+        PlayLoopInternal(soundtrack);
+    }
+
+    public void PlaySound (AudioClip clip) {
+        PlaySoundPitched(clip, 1f);
+    }
+
+    // pitchRange represents a value of 1+
+
+    public void PlaySoundPitched (AudioClip clip, float pitchRange) {
+        pitchRange = Mathf.Max(pitchRange, 1f);
+
+        if (!audioClips.Contains(clip)) {
+            PlayOneShotInternal(clip, pitchRange);
+            audioClips.Add(clip);
+
+            Tweener.Invoke(clip.length, () => {
+                audioClips.Remove(clip);
+            });
+        }
+    }
+
+    private void PlayOneShotInternal (AudioClip clip, float pitchRange) {
+        GameObject audioSourceObject = new GameObject("AudioSource");
+        audioSourceObject.transform.parent = transform;
+
+        AudioSource audioSource = audioSourceObject.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.pitch = Random.Range(1f / pitchRange, pitchRange);
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    private void PlayLoopInternal (AudioClip clip) {
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+}
---
+++ RGB Pixel Shooter/Assets/Scripts/ForRefactoring/BloodSplatterTestScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BloodSplatterTestScript : MonoBehaviour
+{
+    private Vector3 anchorPosition;
+    private Vector3 destinationPosition;
+
+    public Animator animator;
+    private RGBColor color;
+
+    public Material spriteMaterial;
+
+  
+
+
+    public void Initialize( RGBColor enemyColor, HitStatus data, Collision2D col, float emmisionValue)
+    {
+        spriteMaterial = GetComponent<SpriteRenderer>().material;
+        switch (enemyColor)
+        {
+            case RGBColor.RED:
+                spriteMaterial.SetColor("Color_A71320E8", Color.red*emmisionValue);
+                break;
+            case RGBColor.GREEN:
+                spriteMaterial.SetColor("Color_A71320E8", Color.green* emmisionValue);
+                break;
+            case RGBColor.BLUE:
+                spriteMaterial.SetColor("Color_A71320E8", Color.blue* emmisionValue);
+                break;
+            case RGBColor.NONE:
+                spriteMaterial.SetColor("Color_A71320E8", Color.yellow * emmisionValue);
+                break;
+            default:
+                break;
+        }
+
+        this.gameObject.transform.position = col.transform.position;
+
+        if (data.hitColor == HitColor.CORRECT)
+        {
+            animator.SetTrigger("crit");
+
+
+        }
+        if ((data.hitColor == HitColor.NEUTRAL) || (data.hitColor == HitColor.WRONG))
+        {
+            animator.SetTrigger("neutral");
+
+        }
+
+        if (data.damageAmount <= 0 && data.belowThreshold)
+        {
+            animator.SetTrigger("null");
+        }
+
+    }
+
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+        anchorPosition = this.transform.position;
+       
+    }
+
+
+    private void ResetPosition()
+    {
+        this.transform.position = anchorPosition;
+        animator.SetTrigger("reset");
+        FindObjectOfType<ExplosionManager>().ReturnToQueue(this);
+    }
+}
