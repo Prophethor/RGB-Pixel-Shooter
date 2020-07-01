639d66315af752d3f5f164d47c078468942db8f7
Stevan Stojkovic <truper93@gmail.com>
2020-07-01 13:26:32 +0200

    sound work, adding calls to sound and tweaking
     - added sounds to the game
    - added method that allows for overlapping sounds to be playerd PlaySoundOverlapping
    - Commiting this as to not loose it since i fucked up the push from my pc

--- RGB Pixel Shooter/Assets/Scripts/AudioManager.cs
+++ RGB Pixel Shooter/Assets/Scripts/AudioManager.cs
@@ -10,6 +10,10 @@ public class AudioManager : MonoBehaviour {
     private HashSet<AudioClip> audioClips;
     private AudioSource musicSource;
 
+    [Range(0, 1)]
+    public float sfxVolumeValue;
+    public float musicVolumeValue;
+
     private void Awake () {
         if (instance != null) {
             Destroy(gameObject);
@@ -29,9 +33,14 @@ public class AudioManager : MonoBehaviour {
     public void PlaySoundtrack (AudioClip soundtrack) {
         PlayLoopInternal(soundtrack);
     }
+    // stavio sam opcionalni bool koji ako definises kao true, onda mozes da odes u metod koji dozvoljava da se zvuci stakuju
+    public void PlaySound (AudioClip clip, bool overlapingSound = false) {
+        if (overlapingSound)
+        {
+            PlaySoundOverlaping(clip);
+        }
+        else PlaySoundPitched(clip, 1f);
 
-    public void PlaySound (AudioClip clip) {
-        PlaySoundPitched(clip, 1f);
     }
 
     // pitchRange represents a value of 1+
@@ -57,12 +66,25 @@ public class AudioManager : MonoBehaviour {
         audioSource.clip = clip;
         audioSource.pitch = Random.Range(1f / pitchRange, pitchRange);
         audioSource.loop = false;
+        audioSource.volume = sfxVolumeValue;
         audioSource.Play();
+    }
+
+    public void PlaySoundOverlaping(AudioClip clip)
+    {
+
+            PlayOneShotInternal(clip, 1f);
+            audioClips.Add(clip);
+
+            Tweener.Invoke(clip.length, () => {
+                audioClips.Remove(clip);
+            }); 
     }
 
     private void PlayLoopInternal (AudioClip clip) {
         musicSource.clip = clip;
         musicSource.loop = true;
+        musicSource.volume = musicVolumeValue;
         musicSource.Play();
     }
 }
