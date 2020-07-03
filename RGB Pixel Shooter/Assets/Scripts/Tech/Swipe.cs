464eb26c2bc89296b012b5f9f146bfcf4cfb7fca
Veloxis <velox1s@yahoo.com>
2020-07-03 20:20:33 +0200

    Added TouchManager
    TouchManager works as a Singleton with a Publisher pattern (use AddListener and RemoveListenere for Touch events).

--- RGB Pixel Shooter/Assets/Scripts/Tech/Swipe.cs
+++ RGB Pixel Shooter/Assets/Scripts/Tech/Swipe.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class Swipe : MonoBehaviour
-{
+public class Swipe : MonoBehaviour {
     private Vector2 fingerDown;
     private float fingerDownTime;
     private Vector2 fingerUp;
@@ -25,6 +24,10 @@ public class Swipe : MonoBehaviour
         public Vector2 startPos;
         public Vector2 endPos;
         public SwipeDirection direction;
+    }
+
+    private void Start () {
+        TouchManager.GetInstance().AddListener(ProcessTouch);
     }
 
     private void DetectSwipe () {
@@ -62,24 +65,20 @@ public class Swipe : MonoBehaviour
         OnSwipe.Invoke(swipeData);
     }
 
-    private void Update () {
-        foreach (Touch touch in Input.touches) {
-
-            if (touch.phase == TouchPhase.Began) {
-                fingerDownTime = Time.time;
-                fingerDown = touch.position;
-            }
-            if(touch.phase == TouchPhase.Moved && !detectOnlyAfterSwipe) {
-                fingerUp = touch.position;
-                float timePassed = Time.time - fingerDownTime;
-                if (timePassed < .4f) DetectSwipe();
-            }
-            if (touch.phase == TouchPhase.Ended) {
-                fingerUp = touch.position;
-                float timePassed = Time.time - fingerDownTime;
-                if (timePassed < .4f) DetectSwipe();
-            }
+    private void ProcessTouch (Touch touch) {
+        if (touch.phase == TouchPhase.Began) {
+            fingerDownTime = Time.time;
+            fingerDown = touch.position;
+        }
+        if (touch.phase == TouchPhase.Moved && !detectOnlyAfterSwipe) {
+            fingerUp = touch.position;
+            float timePassed = Time.time - fingerDownTime;
+            if (timePassed < .4f) DetectSwipe();
+        }
+        if (touch.phase == TouchPhase.Ended) {
+            fingerUp = touch.position;
+            float timePassed = Time.time - fingerDownTime;
+            if (timePassed < .4f) DetectSwipe();
         }
     }
-
 }
---
+++ RGB Pixel Shooter/Assets/Scripts/Tech/TouchManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+// Should not be manually added to GameObjects
+
+[AddComponentMenu("")]
+public class TouchManager : MonoBehaviour {
+    #region Singleton
+
+    private static TouchManager instance;
+
+    public static TouchManager GetInstance () {
+        if (instance != null) {
+            return instance;
+        }
+
+        GameObject touchManagerObject = new GameObject("TouchManager");
+        instance = touchManagerObject.AddComponent<TouchManager>();
+        instance.listeners = new List<UnityAction<Touch>>();
+
+        return instance;
+    }
+
+    #endregion
+
+    private List<UnityAction<Touch>> listeners;
+
+    public void AddListener (UnityAction<Touch> touchHandler) {
+        listeners.Add(touchHandler);
+    }
+
+    public void RemoveListener (UnityAction<Touch> touchHandler) {
+        listeners.Remove(touchHandler);
+    }
+
+    private void NotifyListeners (Touch touch) {
+        foreach (UnityAction<Touch> action in listeners) {
+            action(touch);
+        }
+    }
+
+    private void Update () {
+        foreach (Touch touch in Input.touches) {
+            NotifyListeners(touch);
+        }
+    }
+}
